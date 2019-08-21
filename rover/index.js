31364a936551a0f5958b0960a9eceab0be2bd820
Gabriel Zsigmond <gaazsig@gmail.com>
2019-08-21 17:16:31 -0300

    added boundaries and move back function

--- rover/index.js
+++ rover/index.js
@@ -5,61 +5,84 @@ let kata = {
   direction: "N",
   compass: ["N", "E", "S", "W"],
   x: 0,
-  y: 0
+  y: 0,
+  travelLog: []
 };
 
-turnRight(kata);
-moveForward(kata);
-moveForward(kata);
-moveForward(kata);
-turnRight(kata);
-moveForward(kata);
-turnLeft(kata);
-moveForward(kata);
+const commands = (string, rover) => {
+  string = string.toUpperCase();
+  commandArray = string.split("");
+  // console.log(commandArray);
+
+  for (let i = 0; i < commandArray.length; i++) {
+    if (commandArray[i] == "F") {
+      moveForward(rover);
+    } else if (commandArray[i] == "B") {
+      moveBackward(kata);
+    } else if (commandArray[i] == "R") {
+      turnRight(kata);
+    } else if (commandArray[i] == "L") {
+      turnLeft(kata);
+    } else {
+      console.log(commandArray[i] + " is NOT a known command");
+    }
+  }
+  console.log(rover);
+};
 
 // ======================
 function turnLeft(rover) {
   let newIndex = 0;
   console.log("turnLeft was called!");
-  // console.log("current direction: " + rover.direction);
   if (rover.direction != "N") {
     newIndex = rover.compass.indexOf(rover.direction) - 1;
   } else {
     newIndex = 3;
   }
   rover.direction = rover.compass[newIndex];
-  // console.log(newIndex);
-  // console.log("new direction: " + rover.direction);
-  // console.log("\n");
 }
 
 function turnRight(rover) {
   let newIndex = 0;
   console.log("turnRight was called!");
-  // console.log("current direction: " + rover.direction);
   if (rover.direction != "W") {
     newIndex = rover.compass.indexOf(rover.direction) + 1;
   } else {
     newIndex = 0;
   }
   rover.direction = rover.compass[newIndex];
-  // console.log(newIndex);
-  // console.log("new direction: " + rover.direction);
-  // console.log("\n");
 }
 
 function moveForward(rover) {
   console.log("moveForward was called");
+  rover.travelLog.push({ X: rover.x, Y: rover.y });
   let direction = rover.direction;
   console.log(direction);
   if (direction == "N") {
-    rover.y = rover.y + 1;
+    rover.y < 9 ? rover.y++ : console.log("You have hit the north boundary");
   } else if (direction == "E") {
-    rover.x = rover.x + 1;
+    rover.x < 9 ? rover.x++ : console.log("You have hit the east boundary");
   } else if (direction == "S") {
-    rover.y = rover.y + 1;
+    rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
   } else if (direction == "W") {
-    rover.x = rover.x + 1;
+    rover.x > 1 ? rover.x++ : console.log("You have hit the west boundary");
   }
-  console.log(rover);
 }
+
+function moveBackward(rover) {
+  console.log("moveBackward was called");
+  rover.travelLog.push({ X: rover.x, Y: rover.y });
+  let direction = rover.direction;
+  console.log(direction);
+  if (direction == "N") {
+    rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
+  } else if (direction == "E") {
+    rover.x > 1 ? rover.x-- : console.log("You have hit the west boundary");
+  } else if (direction == "S") {
+    rover.y < 9 ? rover.y++ : console.log("You have hit the north boundary");
+  } else if (direction == "W") {
+    rover.x < 9 ? rover.x++ : console.log("You have hit the east boundary");
+  }
+}
+
+commands("bb", kata);
