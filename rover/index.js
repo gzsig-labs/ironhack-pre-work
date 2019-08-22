2e5832416357d1967331ce708f0a8a08afcffb66
Gabriel Zsigmond <gaazsig@gmail.com>
2019-08-22 11:50:19 -0300

    most optimized program

--- rover/index.js
+++ rover/index.js
@@ -1,139 +1,119 @@
 // Rover Object Goes Here
 // ======================
+const width = 40;
+const height = 20;
+const dirN = 0;
+const dirE = 1;
+const dirS = 2;
+const dirW = 3;
+const whiteChar = " ";
+const roverChar = ["^", ">", "v", "<"];
 
 let kata = {
-  direction: "N",
-  compass: ["N", "E", "S", "W"],
+  direction: dirN,
   x: 0,
   y: 0,
   travelLog: []
 };
 
-const commands = (string, rover) => {
-  string = string.toUpperCase();
-  commandArray = string.split("");
-  for (let i = 0; i < commandArray.length; i++) {
-    switch (commandArray[i]) {
-      case "F":
-        moveForward(rover);
-        break;
-      case "B":
-        moveBackward(rover);
-        break;
-      case "R":
-        turnRight(rover);
-        break;
-      case "L":
-        turnLeft(rover);
-        break;
-      default:
-        console.log(commandArray[i] + " is NOT a known command");
-    }
-  }
-  rover.travelLog.push({ X: rover.x, Y: rover.y });
-  console.log(rover);
-  showRoute(rover);
+const commandTable = {
+  F: moveForward,
+  B: moveBackward,
+  R: turnRight,
+  L: turnLeft
 };
 
-function turnLeft(rover) {
-  let newIndex = 0;
-  showPoint(rover);
-  console.log("turnLeft was called!");
-  console.log("\n");
-  if (rover.direction != "N") {
-    newIndex = rover.compass.indexOf(rover.direction) - 1;
-  } else {
-    newIndex = 3;
+(() => {
+  kata.travelLog = new Array(height);
+  for (let y = 0; y < height; y++) {
+    let row = new Array(width);
+    for (let x = 0; x < width; x++) {
+      row[x] = whiteChar;
+    }
+    kata.travelLog[y] = row;
   }
-  rover.direction = rover.compass[newIndex];
+  pointer(kata);
+})();
+
+const commands = (string, rover) => {
+  showRoute(rover);
+  
+  string = string.toUpperCase();
+  for (let i = 0; i < string.length; i++) {
+    let f = commandTable[string.charAt(i)];
+    if (f) {
+      f(rover);
+      break
+    } else {
+      console.log(string.charAt(i) + " is NOT a known command");
+    }
+  }
+};
+
+function pointer(rover) {
+  kata.travelLog[kata.y][kata.x] = roverChar[rover.direction];
+}
+
+function turnLeft(rover) {
+  rover.direction = (rover.direction - 1) & 3;
+  pointer(kata);
+  console.log("\nturnLeft was called!");
+  showRoute(rover);
 }
 
 function turnRight(rover) {
-  let newIndex = 0;
-  showPoint(rover);
-  console.log("turnRight was called!");
-  console.log("\n");
-  if (rover.direction != "W") {
-    newIndex = rover.compass.indexOf(rover.direction) + 1;
-  } else {
-    newIndex = 0;
-  }
-  rover.direction = rover.compass[newIndex];
+  rover.direction = (rover.direction + 1) & 3;
+  pointer(kata);
+  console.log("\nturnRight was called!");
+  showRoute(rover);
 }
 
 function moveForward(rover) {
-  rover.travelLog.push({ X: rover.x, Y: rover.y });
-  showPoint(rover);
-  console.log("moveForward was called");
-  console.log("\n");
-  let direction = rover.direction;
-  if (direction == "N") {
-    rover.y < 10 ? rover.y++ : console.log("You have hit the north boundary");
-  } else if (direction == "E") {
-    rover.x < 10 ? rover.x++ : console.log("You have hit the east boundary");
-  } else if (direction == "S") {
-    rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
-  } else if (direction == "W") {
-    rover.x > 1 ? rover.x++ : console.log("You have hit the west boundary");
+  console.log("\nmoveForward was called!");
+  switch (rover.direction) {
+    case dirN:
+      rover.y < (height - 1) ? rover.y++ : console.log("You have hit the north boundary");
+      break;
+    case dirE:
+      rover.x < (width - 1) ? rover.x++ : console.log("You have hit the east boundary");
+      break;
+    case dirS:
+      rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
+      break;
+    case dirW:
+      rover.x > 1 ? rover.x-- : console.log("You have hit the west boundary");
+      break;
   }
+  pointer(kata);
+  showRoute(rover);
 }
 
 function moveBackward(rover) {
-  rover.travelLog.push({ X: rover.x, Y: rover.y });
-  showPoint(rover);
-  console.log("moveBackward was called");
-  console.log("\n");
-  let direction = rover.direction;
-  if (direction == "N") {
-    rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
-  } else if (direction == "E") {
-    rover.x > 1 ? rover.x-- : console.log("You have hit the west boundary");
-  } else if (direction == "S") {
-    rover.y < 10 ? rover.y++ : console.log("You have hit the north boundary");
-  } else if (direction == "W") {
-    rover.x < 10 ? rover.x++ : console.log("You have hit the east boundary");
+  console.log("\nmoveBackward was called!");
+  switch (rover.direction) {
+    case dirS:
+      rover.y < (height - 1) ? rover.y++ : console.log("You have hit the north boundary");
+      break;
+    case dirW:
+      rover.x < (width - 1) ? rover.x++ : console.log("You have hit the east boundary");
+      break;
+    case dirN:
+      rover.y > 1 ? rover.y-- : console.log("You have hit the south boundary");
+      break;
+    case dirE:
+      rover.x > 1 ? rover.x-- : console.log("You have hit the west boundary");
+      break;
   }
-}
-
-function showPoint(rover) {
-  for (let i = 0; i < 10; i++) {
-    let row = [];
-    for (let j = 0; j < 10; j++) {
-      if (rover.y == i && rover.x == j) {
-        row.push("|X");
-      } else {
-        row.push("| ");
-      }
-    }
-    console.log(row.join(""));
-  }
+  pointer(kata);
+  showRoute(rover);
 }
 
 function showRoute(rover) {
-  let coordinates = rover.travelLog;
-  let orderedLogs = []
-  let cont = 0
-  while(cont < 10){
-  coordinates.forEach(log => {
-    if (log.Y == cont) {
-      orderedLogs.push(log);
-    }
-  });
-  cont++
+  for (let i = height - 1; i >= 0; i--) {
+    console.log("|" + rover.travelLog[i].join("|") + "|");
   }
-
-console.log(orderedLogs);
-
-
-  let map = [];
-  for (let i = 0; i < 10; i++) {
-    let row = [];
-    for (let i = 0; i < 10; i++) {
-      row.push("| ");
-    }
-    map.push(row);
-  }
-  console.log(map);
 }
 
-commands("ffrfffrbblffrfffff", kata);
+commands("", kata);
+
+
