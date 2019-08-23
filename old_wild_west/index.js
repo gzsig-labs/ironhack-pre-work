d7dd7bd3fee1f0c0b949149954ac85d1325014a9
Gabriel Zsigmond <gaazsig@gmail.com>
2019-08-23 19:07:04 -0300

    working for all scenarios

--- old_wild_west/index.js
+++ old_wild_west/index.js
@@ -1,79 +1,49 @@
 function dirReduc(dir) {
-  // console.log(dir);
-  let groupObj = dirGroup(dir);
-  // console.log(groupObj);
-  let res = setUpFinalArray(groupObj);
-  console.log(res);
+  let reduceDir = [];
+  reduceDir = reduce(dir);
+
+  while (reduceDir[1] == "again") {
+    reduceDir = reduce(reduceDir[0]);
+  }
+  console.log(dir);
+  console.log(reduceDir);
 }
 
-function dirGroup(dir) {
-  var counts = {};
-  dir.forEach(x => {
-    counts[x] = (counts[x] || 0) + 1;
-  });
-  return counts;
-}
-
-function setUpFinalArray(groupObj) {
-  let n = 0;
-  let e = 0;
-  let s = 0;
-  let w = 0;
-  if (Object.values(groupObj).every((val, i, arr) => val === arr[0])) {
-    return [];
-  } else {
-    if (groupObj["NORTH"] > groupObj["SOUTH"]) {
-      n = groupObj["NORTH"] - groupObj["SOUTH"];
-    } else if (groupObj["NORTH"] < groupObj["SOUTH"]) {
-      s = groupObj["SOUTH"] - groupObj["NORTH"];
+function reduce(dir) {
+  let res = [];
+  for (i = 0; i < dir.length; i++) {
+    if (dir[i] == "NORTH" && dir[i + 1] != "SOUTH") {
+      res.push(dir[i]);
+    } else if (dir[i] == "NORTH" && dir[i + 1] == "SOUTH") {
+      i++;
+      continue;
     }
-    if (groupObj["WEST"] > groupObj["EAST"]) {
-      w = groupObj["WEST"] - groupObj["EAST"];
-    } else if (groupObj["WEST"] < groupObj["EAST"]) {
-      e = groupObj["EAST"] - groupObj["WEST"];
+    if (dir[i] == "EAST" && dir[i + 1] != "WEST") {
+      res.push(dir[i]);
+    } else if (dir[i] == "EAST" && dir[i + 1] == "WEST") {
+      i++;
+      continue;
+    }
+    if (dir[i] == "SOUTH" && dir[i + 1] != "NORTH") {
+      res.push(dir[i]);
+    } else if (dir[i] == "SOUTH" && dir[i + 1] == "NORTH") {
+      i++;
+      continue;
+    }
+    if (dir[i] == "WEST" && dir[i + 1] != "EAST") {
+      res.push(dir[i]);
+    } else if (dir[i] == "WEST" && dir[i + 1] == "EAST") {
+      i++;
+      continue;
     }
   }
-  return createArray([n, e, s, w]);
+  if (res.length == dir.length) {
+    return res;
+  } else {
+    return [res, "again"];
+  }
 }
 
-function createArray(dirCount) {
-  let res = [];
-  dirCount.forEach((n, i) => {
-    for (var j = 0; j < n; j++) {
-      if (i == 0) {
-        res.push("NORTH");
-      } else if (i == 1) {
-        res.push("EAST");
-      } else if (i == 2) {
-        res.push("SOUTH");
-      } else if (i == 3) {
-        res.push("WEST");
-      }
-    }
-  });
-  return res;
-}
-
-const dirArray = [
-  "NORTH",
-  "SOUTH",
-  "SOUTH",
-  "SOUTH",
-  "EAST",
-  "EAST",
-  "WEST",
-  "NORTH",
-  "WEST",
-  "WEST",
-  "NORTH",
-  "WEST",
-  "NORTH",
-  "WEST",
-  "NORTH",
-  "WEST",
-  "NORTH",
-  "WEST",
-  "NORTH"
-];
+const dirArray = ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"];
 
 dirReduc(dirArray);
